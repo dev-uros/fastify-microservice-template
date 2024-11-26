5ac99c5eb2c6c7445425ca7d03f33206cfda687a
dev-uros <uros.minic@kentkart.rs>
2024-11-26 15:46:00 +0100

    Adding db closing on server fail

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,8 @@ closeWithGrace(async ({ err }) => {
     if (err) {
         app.log.error({ err }, 'server closing due to error')
     }
+    app.log.info('Closing db connections')
+    await app.db.destroy();
     app.log.info('shutting down gracefully')
     await app.close()
 })
